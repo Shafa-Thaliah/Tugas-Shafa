767d30bc85716b61e9c57a6a94d3c091f87c0f03
Shafa-Thaliah <72061665+Shafa-Thaliah@users.noreply.github.com>
2020-09-30 11:07:55 +0700

    Add files via upload

---
+++ Tugas2.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Faa
+ */
+import java.util.*;
+public class Tugas2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         int input;
+        boolean isTrue = false;
+        Scanner user = new Scanner(System.in);
+
+ System.out.println("Masukkan angka -->");
+                input = user.nextInt();
+                if (input % 2 == 0) {
+                    System.out.println(input + " Adalah Bilangan Genap");
+                 
+                } else {
+                    System.out.println(input + " Adalah Bilangan Ganjil");
+                }
+
+
+
+
+}
+
+
+
+
+
+
+}
+    
